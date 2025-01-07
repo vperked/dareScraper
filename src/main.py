f02fe6412fb7d7608689fddd6c6fe84848b70f6d
vperked <perked@riseup.net>
2025-01-06 21:06:33 -0500

    Almost fully revamped mail, added error message classes & the likes. I cant read my own code right now so im gonna take a break, staring at my screen is taking a toll...

--- src/main.py
+++ src/main.py
@@ -1,34 +1,65 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import InvalidArgumentException
 class Setup:
-    def initCrawler():
-        options = Options()
-        options.add_argument("--headless") 
-        fireFoxDriver = webdriver.Firefox(options=options)
-        return fireFoxDriver
+    def initGoogleCrawler():
+        try:
+            options = Options()
+            arugementArray = ["--headless", "--disable-gpu", "--disable-blink-features=AutomationControlled" ]
+            options.add_argument(arugementArray[0])
+            options.add_argument(arugementArray[1])
+            options.add_argument(arugementArray[2])
+            chromeDriver = webdriver.Chrome(options=options)
+            return chromeDriver
+        except Exception as e:
+            Messages.Errors.errorMessage(e)
+    def initFireFoxCrawler():
+        try:    
+            options = Options()
+            options.add_argument("--headless") 
+            fireFoxDriver = webdriver.Firefox(options=options)
+            return fireFoxDriver
+        except Exception as e:
+            Messages.Errors.errorMessage(error=e)
+    def startUp(chrome, fireFox):
+            print("c for chrome, f for firefox.")
+            uI = input("Welcome to DareScraper:")
+            if uI == "c":
+                chrome = Setup.initGoogleCrawler()
+            elif uI == "f":
+                fireFox = Setup.initFireFoxCrawler()
+class Crawling: 
+    def crawlFireFox(fireFoxDriver):
+        try:
+            website = "https://github.com"
+            fireFoxDriver.get(website)
+            fireFoxDriver.set_page_load_timeout(5)
+            return True
+        except InvalidArgumentException as invalidDomain:
+            Messages.Errors.errorMessage(f"Invalid Domain Dipshit: {invalidDomain}.")
+        except Exception as e:
+            Messages.Errors.errorMessage(error=e)
+            return False
     def beganCrawling(fireFoxDriver):
         result = Crawling.crawlSite(fireFoxDriver=fireFoxDriver)
         if result == True:
             print("Done")
         else:
             print("Failed!")
-class Crawling:
-    def crawlSite(fireFoxDriver):
-        try:
-            fireFoxDriver.get("http://localhost:5000/api/ip")
-            fireFoxDriver.set_page_load_timeout(5)
-            return True
-        except Exception as e:
-            print(f"{e}")
-            return False
-    
-
+class WhenActive:
+    def doThis():
+        print("logic")
+class Vaild:
+    def isDomainValid():
+        print("Logic")
+class Messages:
+    class Errors:
+        def errorMessage(error):
+            print(f"There was a error: {error}")
+        def sucessMessage(website):
+            print(f"Crawled {website}")
 
 if __name__ == "__main__":
-    setup = Setup()
-    driver = Setup.initCrawler()
-    result = Setup.beganCrawling(driver)
-    if result == False:
-        driver.quit()
+    Setup.startUp()
 
