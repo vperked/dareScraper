5480f94aed864b162cca0ac831a4f2b2d3d2c823
vperked <perked@riseup.net>
2025-01-07 20:14:54 -0500

    Too much to list, program doesnt work atm tho... gimme like 2 or hours and ill COOK.

---
+++ src/functions/database.py
@@ -0,0 +1,36 @@
+import mysql.connector
+import json
+from main import Messages
+class Json: ## Pretty much contains the same function pulling different data, im not gonna bother noting all of it.
+    def pullDBIP():
+        with open("config.json") as config:
+            configFile = json.load(config)
+            dataBaseIp = configFile["databaseIP"]
+            return dataBaseIp
+    def pullDBPort():
+        with open ("config.json") as config:
+            configFile = json.load(config)
+            databasePort = configFile["databasePort"]
+            return databasePort
+    def pullDBUser():
+        with open ("config.json") as config:
+            configFile = json.load(config)
+            databaseUser = configFile["databaseUser"]
+            return databaseUser
+    def getDBPassword():
+        password = input("Please input your mySQL DB password:") # Grabs the user input 
+        return password # Returns the password
+ 
+ 
+class Database:
+    def connectToDataBase():
+        try:
+            connection = mysql.connector.connect(
+            host=Json.pullDBIP(),
+            port=Json.pullDBPort(),
+            user=Json.pullDBUser(),
+            password=Json.getDBPassword()
+    )
+        except Exception as e:
+            Messages.Errors.errorMessage(e)
+
--- src/main.py
+++ src/main.py
@@ -1,113 +1,151 @@
 import json
 import sys
+import subprocess
+import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import InvalidArgumentException
 from selenium.webdriver.common.utils import find_connectable_ip
-class Setup: ## Contains things used for setting up dareScraper.
-    ## If user chooses, use the Google Driver.
+class Setup:  ## Contains things used for setting up DareScraper.
+    @staticmethod
+    def aptUpdate():
+        try:
+            print("Updating your system packages...")
+            cmd = subprocess.run(["sudo", "apt", "update"])
+            if cmd.returncode == 0:
+                print(f"Done updating, upgrading now...")
+                return Setup.aptUpgrade()
+            else:
+                sys.exit()
+        
+        except Exception as e:
+            Messages.Errors.errorMessage(e)
+            return False
+    @staticmethod
+    def aptUpgrade():
+        try:
+            print("Now we upgrade...")
+            cmd = subprocess.run(["sudo", "apt", "upgrade", "-y"])
+            if cmd.returncode == 0:
+                print("Done upgrading")
+                return True
+            else:
+                print("All programs updated...")
+            return True
+        except Exception as e:
+            Messages.Errors.errorMessage(e)
+            return False
+    @staticmethod
     def initConfig():
-        with open ("config.json") as config:
-            website = json.load(config)
-            return website["website"]
-    def resolveWebsite(website):
+        try:
+            with open("config.json") as config:
+                website = json.load(config)
+                return website["website"]
+        except Exception as e:
+            Messages.Errors.errorMessage(e)
+    @staticmethod
+    def resolveWebsite():
         try:    
             website = Setup.initConfig()
             return find_connectable_ip(website)
         except Exception as e:
             Messages.Errors.errorMessage(e)
+    @staticmethod
     def initGoogleCrawler():
         try:
             options = Options() 
-            argumentArray = ["--headless", "--disable-gpu", "--disable-blink-features=AutomationControlled" ]
-            options.add_argument(argumentArray[0])
-            options.add_argument(argumentArray[1])
-            options.add_argument(argumentArray[2])
+            argumentArray = ["--headless", "--disable-gpu", "--disable-blink-features=AutomationControlled"]
+            for arg in argumentArray:
+                options.add_argument(arg)
             chromeDriver = webdriver.Chrome(options=options)
+            print("Chrome Driver Initialized, continuing...")
             return chromeDriver
         except Exception as e:
-            Messages.Errors.errorMessage(e) # if error return the message
-    ## If user chooses, use the FireFox Driver.
-    def initFireFoxCrawler(website):
+            Messages.Errors.errorMessage(e)  # if error return the message
+    @staticmethod
+    def initFireFoxCrawler():
         try:    
             options = Options()
-            options.add_argument("--headless") # make it headless 
+            options.add_argument("--headless")  # make it headless
             fireFoxDriver = webdriver.Firefox(options=options)
+            print("Firefox Driver Initialized, continuing...")
             return fireFoxDriver
         except Exception as e:
-            Messages.Errors.errorMessage(error=e) # if error, return message
+            Messages.Errors.errorMessage(error=e)  # if error, return message
+    @staticmethod
     def startUp(chromeDriver, fireFoxDriver):
         while True:
             print("c for chrome, f for firefox.")
-            uI = input("Welcome to DareScraper:") # Grab user input 
+            uI = input("Welcome to DareScraper:")  # Grab user input 
             try:    
-                ui = str(uI)
+                str(uI)
             except:
                 print("Invalid")
                 return
-            if uI == "c": ## if they respond with C
-                Setup.initGoogleCrawler() ## Call the setup class & init the Google Driver.
+            if uI == "c":  # if they respond with C
+                Setup.initGoogleCrawler()  # Corrected call
                 Crawling.beganGoogleCrawling(chromeDriver)
-            elif uI == "f": ## if they respond with F
-                Setup.initFireFoxCrawler(website=Setup.initConfig()) ## Call the setup class & init the FireFox Driver.
+            elif uI == "f":  # if they respond with F
+                Setup.initFireFoxCrawler()  # Corrected call
                 Crawling.beganfireFoxCrawling(fireFoxDriver)
-class Crawling: # Contains the actual crawling. 
+class Crawling:  # Contains the actual crawling.
+    @staticmethod
     def crawlFireFox(fireFoxDriver):
         try:
-            fireFoxDriver.get(Setup.initConfig()) # Get the website
-            fireFoxDriver.set_page_load_timeout(5) ## set the page timeout
-            Setup.resolveWebsite(website=Setup.initConfig())
+            fireFoxDriver.get(Setup.initConfig())  # Get the website
+            fireFoxDriver.set_page_load_timeout(5)  ## set the page timeout
+            Setup.resolveWebsite()
             return True
-        except InvalidArgumentException as invalidDomain: # if there is no domain, or it is invalid it will return invalid domain.
-            Messages.Errors.errorMessage(f"Invalid Domain Dipshit: {invalidDomain}.") # Error Message
-        except Exception as e: ## Normal shit
+        except InvalidArgumentException as invalidDomain:  # if there is no domain, or it is invalid it will return invalid domain.
+            Messages.Errors.errorMessage(f"Invalid Domain Dipshit: {invalidDomain}.")  # Error Message
+        except Exception as e:  ## Normal shit
             Messages.Errors.errorMessage(error=e)
-            return False ## if fails return false so program cannot contiune on.
+            return False  ## if fails return false so program cannot continue on.
+    @staticmethod
     def crawlGoogle(googleDriver):
         try:
             googleDriver.get(Setup.initConfig())
-            googleDriver.set_page_load_timeout(3) # same as above, just with a different driver.
-            ip = Setup.resolveWebsite(website=Setup.initConfig())
-            if ip == None:
+            googleDriver.set_page_load_timeout(3)  # same as above, just with a different driver.
+            ip = Setup.resolveWebsite()
+            if ip is None:
                 print("No IP found.")
             elif ip == str:
                 print(ip)
             return True
-        except InvalidArgumentException as invalidDomain: # if there is no domain, or it is invalid it will return invalid domain.
-            Messages.Errors.errorMessage(f"Invalid Domain Dipshit: {invalidDomain}.") # Error Message
-        except Exception as e: ## Normal shit
+        except InvalidArgumentException as invalidDomain:  # if there is no domain, or it is invalid it will return invalid domain.
+            Messages.Errors.errorMessage(f"Invalid Domain Dipshit: {invalidDomain}.")  # Error Message
+        except Exception as e:  ## Normal shit
             Messages.Errors.errorMessage(error=e)
-            return False ## if fails return false so program cannot contiune on.
+            return False  ## if fails return false so program cannot continue on.
+    @staticmethod
     def beganGoogleCrawling(chromeDriver):
-        result = Crawling.crawlGoogle(googleDriver=chromeDriver) # Actually call the function and began crawling
-        if result == True:
-            Messages.Errors.sucessMessage(Setup.initConfig()) ## Send sucess message.
-            sys.exit() ## Uses sys exit for clean exiting
+        result = Crawling.crawlGoogle(googleDriver=chromeDriver)  # Actually call the function and begin crawling
+        if result:
+            Messages.Errors.sucessMessage(Setup.initConfig())  # Send success message.
+            sys.exit()  # Uses sys exit for clean exiting
         else:
             print("Failed")
+    @staticmethod
     def beganfireFoxCrawling(fireFoxDriver):
-        result = Crawling.crawlFireFox(fireFoxDriver=fireFoxDriver) # same thing as above.
-        if result == True:
+        result = Crawling.crawlFireFox(fireFoxDriver=fireFoxDriver)  # same thing as above.
+        if result:
             Messages.Errors.sucessMessage(Setup.initConfig())
             sys.exit()  # same as above
         else:
             print("Failed!")
-class WhenActive:
-    def doThis():
-        print("logic")
-class Vaild:
-    def isDomainValid():
-        print("Logic")
 class Messages:
     class Errors:
+        @staticmethod
         def errorMessage(error):
-            print(f"There was a error: {error}")
+            print(f"There was an error: {error}")
+
+        @staticmethod
         def sucessMessage(website):
             print(f"Crawled {website}")
-
 if __name__ == "__main__":
-    chromeDriver = Setup.initGoogleCrawler()
-    fireFoxDriver = Setup.initFireFoxCrawler(website=Setup.initConfig())
-    Setup.startUp(chromeDriver, fireFoxDriver)
-
+    try:
+        chromeDriver = Setup.initGoogleCrawler()
+        fireFoxDriver = Setup.initFireFoxCrawler()
+        Setup.startUp(chromeDriver, fireFoxDriver)
+    except Exception as e:
+        Messages.Errors.errorMessage(e)
